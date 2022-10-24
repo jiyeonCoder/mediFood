de36cf9b2d7e9a32fe9d5de6c1e1fd4a301f7c3e
Jiyeon <71193553+jiyeonCoder@users.noreply.github.com>
2022-10-25 00:50:05 +0900

    Create DetailPage.js

---
+++ DetailPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+
+const DetailPage = (props) => {
+  return (
+    <div>
+      <h1>This is Detail Page.</h1>
+    </div>
+  );
+};
+
+export default DetailPage;
