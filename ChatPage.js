d4c43ffa5704740e6f5e6bbeaf1a5e504f538b5b
Jiyeon <71193553+jiyeonCoder@users.noreply.github.com>
2022-10-25 00:50:45 +0900

    Create ChatPage.js

---
+++ ChatPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+
+const Chat = (props) => {
+  return (
+    <div>
+      <h1>This is Chat Page.</h1>
+    </div>
+  );
+};
+
+export default Chat;
