71b39a5b3fdf82d574425c882ac87245592f3792
Jiyeon <71193553+jiyeonCoder@users.noreply.github.com>
2022-10-25 00:50:28 +0900

    Create ChatUploadPage.js

---
+++ ChatUploadPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+
+const ChatUploadPage = (props) => {
+  return (
+    <div>
+      <h1>This is the Chat Upload Page.</h1>
+    </div>
+  );
+};
+
+export default ChatUploadPage;
