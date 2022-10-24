a2bd2e6ba000ae2e8ee1807acc80057c964e1c35
Jiyeon <71193553+jiyeonCoder@users.noreply.github.com>
2022-10-25 00:51:25 +0900

    Create AdminPage.js

---
+++ AdminPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+
+const AdminPage = (props) => {
+  return (
+    <div>
+      <h1>This is the Admin Page.</h1>
+    </div>
+  );
+};
+
+export default AdminPage;
